e20f078ad603f2d417bfa019ef94ad6ce4367dce
Shishir <shishirk_003@yahoo.com>
2021-09-12 20:27:40 +0530

    Working basic API example completed

---
+++ ApiTesting/ApiTesting/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApiTesting.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
